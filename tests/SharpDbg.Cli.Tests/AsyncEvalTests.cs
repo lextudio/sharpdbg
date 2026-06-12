9abca0984fa987f1959d2b22426cec0c71769c5f
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2026-06-13 02:08:57 +1000

    AsyncEvalTests - test after suspension point

--- tests/SharpDbg.Cli.Tests/AsyncEvalTests.cs
+++ tests/SharpDbg.Cli.Tests/AsyncEvalTests.cs
@@ -9,7 +9,7 @@ public class AsyncEvalTests(ITestOutputHelper testOutputHelper)
 	[Fact]
 	public async Task AsyncMethod_EvaluationRequest_Returns()
 	{
-		var startSuspended = false;
+		var startSuspended = true;
 
 		var (debugProtocolHost, initializedEventTcs, debugEventTcs, adapter, p2) = TestHelper.GetRunningDebugProtocolHostInProc(testOutputHelper, startSuspended);
 		using var _ = adapter;
@@ -20,12 +20,15 @@ public class AsyncEvalTests(ITestOutputHelper testOutputHelper)
 			.WithInitializeRequest()
 			.WithAttachRequest(p2.Id)
 			.WaitForInitializedEvent(initializedEventTcs);
+
+		var breakpointFilePath = Path.JoinFromGitRoot("tests", "DebuggableConsoleApp", "AsyncMethodEvalClass.cs");
 		debugProtocolHost
-			.WithBreakpointsRequest([10, 12], Path.JoinFromGitRoot("tests", "DebuggableConsoleApp", "AsyncMethodEvalClass.cs"))
+			.WithBreakpointsRequest([10, 12], breakpointFilePath)
 			.WithConfigurationDoneRequest()
 			.WithOptionalResumeRuntime(p2.Id, startSuspended);
 
 		var stoppedEvent = await debugProtocolHost.WaitForStoppedEvent(debugEventTcs);
+		stoppedEvent.ReadStopInfo().Should().Be((breakpointFilePath, 10, 3));
 		debugProtocolHost
 			.WithStackTraceRequest(stoppedEvent.ThreadId!.Value, out var stackTraceResponse)
 			.WithScopesRequest(stackTraceResponse.StackFrames!.First().Id, out var scopesResponse);
@@ -37,9 +40,25 @@ public class AsyncEvalTests(ITestOutputHelper testOutputHelper)
 
 		variables.Should().HaveCount(2);
 
-		var stackFrameId = stackTraceResponse.StackFrames!.First().Id;
-		debugProtocolHost.WithEvaluateRequest(stackFrameId, "localInt", out var evaluateResponse);
-		evaluateResponse.Result.Should().Be("4");
+		TestVariablesEval(stackTraceResponse.StackFrames!.First().Id);
 
+		// Continue, so we hit the breakpoint after the async suspension point
+		debugProtocolHost.WithContinueRequest();
+
+		var stoppedEvent2 = await debugProtocolHost.WaitForStoppedEvent(debugEventTcs);
+		stoppedEvent2.ReadStopInfo().Should().Be((breakpointFilePath, 12, 3));
+		debugProtocolHost.WithStackTraceRequest(stoppedEvent2.ThreadId!.Value, out var stackTraceResponse2);
+
+		TestVariablesEval(stackTraceResponse2.StackFrames!.First().Id);
+		return;
+
+		void TestVariablesEval(int stackFrameId)
+		{
+			debugProtocolHost.WithEvaluateRequest(stackFrameId, "localInt", out var evaluateResponse);
+			evaluateResponse.Result.Should().Be("4");
+
+			debugProtocolHost.WithEvaluateRequest(stackFrameId, "IntField", out var evaluateResponse2);
+			evaluateResponse2.Result.Should().Be("10");
+		}
 	}
 }
